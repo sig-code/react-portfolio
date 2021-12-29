864f52b6e83b8e5d027c59a513168c904594902d
takuya-mashimo <m.takku29@gmail.com>
2021-12-29 17:14:09 +0900

    コンタクトコンポーネントの追加・スキルコンポーネントのスタイル適用

---
+++ src/components/Contact.jsx
@@ -0,0 +1,18 @@
+export const Contact = () => {
+  return (
+    <div id='contact' className='contact'>
+      <div className='container'>
+        <div className='contact-contents'>
+          <h2>Contact to Me!</h2>
+          <p className='description'>
+            お仕事のご相談・ご依頼など下記よりご連絡ください。
+          </p>
+          {/* TODO: GoogleFormのURLに変更する */}
+          <a href='https://twitter.com/taku_enjineer' className='btn'>
+            Contact Us
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
